907432a2eb665a892de055da7883b7ee7976673c
Ehsan Tavakoli <ehsan.tavakoli@gmail.com>
2018-05-06 16:12:06 +1000

    user register api passed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs')
 // Load User Model
 const Users = require('../../models/User');
 
@@ -19,11 +20,30 @@ router.post('/register', (req, res) => {
 			if (user) {
 				return res.status(400).json({ email: 'email already exists'});
 			} else {
+				const avatar = gravatar.url(req.body.email, {
+					s: '200', // size
+					r: 'PG', // Rating
+					d: 'mm' // Default
+				});
+
 				const newUser = new User({
 					name: req.body.name,
 					email: req.body.email,
 					avatar,
 					password: req.body.password
+				});
+
+				bcrypt.genSalt(10, (err, salt) => {
+					bcrypt.hash(newUser.password, salt, (err, hash) => {
+						if (err) {
+							throw err;
+						}
+						newUser.password = hash;
+
+						newUser.save()
+							.then(user => res.json(user))
+							.catch(err => console.log(err));
+					})
 				});
 			}
 		})
