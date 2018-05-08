317de4667eb242c16b18c12a80cb82da6518552f
Ehsan Tavakoli <ehsan.tavakoli@gmail.com>
2018-05-08 18:33:31 +1000

    adds basic react comp.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
+
+import Landing from './components/layout/Landing';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<NavBar />
+				<Landing />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
